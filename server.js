abf8fbbdf42bdabc20af34652498d65475cfc843
Jose Italo <jose_italo_PF021@hotmail.com>
2024-11-07 19:21:49 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const {join} = require("node:path")
+const {createServer} = require("node:http")
+const { Server } = require("socket.io")
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+app.use(express.static(join(__dirname+"/public")))
+// app.get("/", (req, res)=>{
+//     res.sendFile(join(__dirname, '/public/index.html'))
+// })
+
+io.on('connection', (socket) => {
+    socket.on("newUser", (Username)=>{
+        socket.broadcast.emit("update", Username)
+    })
+    socket.on("exitUser", (Username)=>{
+        socket.broadcast.emit("exit", Username)
+    })
+    socket.on("chat_message", (chatMensage)=>{
+     socket.broadcast.emit("chat", chatMensage)
+    })
+})
+
+server.listen(5000, ()=>{
+    console.log('Server running at port 5000')
+})
